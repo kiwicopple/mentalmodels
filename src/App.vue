8145f50231d2609fd4a3eb53926d9cb804e8eeb7
Copple <pcopplestone@gmail.com>
2017-12-30 22:50:20 +0800

    add to homescreen

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,7 @@
         <h3 class="md-caption">About</h3>
         <p>Mental models are "core concepts". They unchanging fundamentals for a particular subject.<br> Learning these concepts will give you a better comprehension of the world and help you make rational decisions.</p>
         <p>This app is a list of 113 mental models, grouped into 8 color-coded categories. It will randomly show one mental model as a "flash card", with the description on the "back".</p>
-        <p>Try to do a few each day. Spaced repetition will help you remember. </p>
+        <p>Try to do a few each day. Spaced repetition will help you remember. If you are using a mobile you can add this page to your homescreen and use it offline.</p>
       </md-content>
 
       <md-content>
